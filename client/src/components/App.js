f45bcff7faa0fbb78968dbb43514630f99a95cb4
Alexandra <alexandraneff@live.com>
2023-07-12 08:08:41 -0500

    added base content to components/pages'

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import NavBar from './NavBar'
+import Login from '../pages/Login'
+import ItemList from '../pages/ItemList'
+import NewReview from '../pages/NewReview'
 
 function App() {
   return (
-    <div>
-      Hello 
+   <>
       <NavBar />
-    </div>
-
+      <main>
+        <Routes>
+          <Route path="/new"
+            element={<NewReview />}
+          />
+          <Route path="/"
+            element={<ItemList />}
+          />
+        </Routes>
+      </main>
+   </>
   );
 }
 
