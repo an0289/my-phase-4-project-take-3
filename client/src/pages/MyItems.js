a5e7bf08315994d85f6759e0a0978535a45463b6
Alexandra <alexandraneff@live.com>
2023-07-17 09:28:07 -0500

    added grid and items styling to MyItems

--- client/src/pages/MyItems.js
+++ client/src/pages/MyItems.js
@@ -1,7 +1,94 @@
 import React from 'react'
+import { Grid, Image, Item, Icon } from 'semantic-ui-react'
 
 function MyItems() {
-
+    return (
+        <Grid columns={3} divided>
+            <Grid.Row>
+                <Grid.Column>
+                    <Item>
+                        <Item.Image size='small' src='/images/wireframe/image.png' />
+    
+                        <Item.Content>
+                            <Item.Header as='a'>Cute Dog</Item.Header>
+                            <Item.Description>paragraph</Item.Description>
+                            <Item.Extra>
+                            <Icon color='green' name='check' /> 121 Votes
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Grid.Column>
+                <Grid.Column>
+                    <Item>
+                        <Item.Image size='small' src='/images/wireframe/image.png' />
+    
+                        <Item.Content>
+                            <Item.Header as='a'>Cute Dog</Item.Header>
+                            <Item.Description>paragraph</Item.Description>
+                            <Item.Extra>
+                            <Icon color='green' name='check' /> 121 Votes
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Grid.Column>
+                <Grid.Column>
+                    <Item>
+                        <Item.Image size='small' src='/images/wireframe/image.png' />
+    
+                        <Item.Content>
+                            <Item.Header as='a'>Cute Dog</Item.Header>
+                            <Item.Description>paragraph</Item.Description>
+                            <Item.Extra>
+                            <Icon color='green' name='check' /> 121 Votes
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Grid.Column>
+            </Grid.Row>
+    
+            <Grid.Row>
+                <Grid.Column>
+                    <Item>
+                        <Item.Image size='small' src='/images/wireframe/image.png' />
+    
+                        <Item.Content>
+                            <Item.Header as='a'>Cute Dog</Item.Header>
+                            <Item.Description>paragraph</Item.Description>
+                            <Item.Extra>
+                            <Icon color='green' name='check' /> 121 Votes
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Grid.Column>
+                <Grid.Column>
+                    <Item>
+                        <Item.Image size='small' src='/images/wireframe/image.png' />
+    
+                        <Item.Content>
+                            <Item.Header as='a'>Cute Dog</Item.Header>
+                            <Item.Description>paragraph</Item.Description>
+                            <Item.Extra>
+                            <Icon color='green' name='check' /> 121 Votes
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Grid.Column>
+                <Grid.Column>
+                    <Item>
+                        <Item.Image size='small' src='/images/wireframe/image.png' />
+    
+                        <Item.Content>
+                            <Item.Header as='a'>Cute Dog</Item.Header>
+                            <Item.Description>paragraph</Item.Description>
+                            <Item.Extra>
+                            <Icon color='green' name='check' /> 121 Votes
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Grid.Column>
+            </Grid.Row>
+      </Grid>
+        )
 }
 
 export default MyItems
--- client/src/pages/NewReview.js
+++ client/src/pages/NewReview.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+// This will likely change to 'NewItem' so that a review can be submitted directly from the Home page
 function NewReview() {
 
 }
