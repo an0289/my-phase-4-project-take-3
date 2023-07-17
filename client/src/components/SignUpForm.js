4749686987f6bf5d2ec9d2a83c34272d6c883c2f
Alexandra <alexandraneff@live.com>
2023-07-17 09:45:32 -0500

    added signup form

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Button, Form, Segment, Divider } from 'semantic-ui-react'
 
 function SignUpForm() {
-
+    return (
+        <>
+        <Divider hidden />
+        <Segment inverted>
+            <Form inverted>
+                <Form.Group widths='equal'>
+                    <Form.Input fluid label='First name' placeholder='First name' />
+                    <Form.Input fluid label='Last name' placeholder='Last name' />
+                </Form.Group>
+                <Button type='submit'>Sign Up</Button>
+            </Form>
+        </Segment>
+        </>
+        )
 }
 
 export default SignUpForm
