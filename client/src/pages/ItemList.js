abeb0a38a1960573d86e84b696f2b0271284a24e
Alexandra <alexandraneff@live.com>
2023-07-17 09:18:57 -0500

    added items styling to ItemList

--- client/src/pages/ItemList.js
+++ client/src/pages/ItemList.js
@@ -1,30 +1,90 @@
 import React from 'react'
-import { Grid, Image, Item } from 'semantic-ui-react'
+import { Grid, Image, Item, Icon } from 'semantic-ui-react'
 
 function ItemList() {
     return (
     <Grid columns={3} divided>
         <Grid.Row>
             <Grid.Column>
-                <Image src='/images/wireframe/media-paragraph.png' />
+                <Item>
+                    <Item.Image size='small' src='/images/wireframe/image.png' />
+
+                    <Item.Content>
+                        <Item.Header as='a'>Cute Dog</Item.Header>
+                        <Item.Description>paragraph</Item.Description>
+                        <Item.Extra>
+                        <Icon color='green' name='check' /> 121 Votes
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
             </Grid.Column>
             <Grid.Column>
-                <Image src='/images/wireframe/media-paragraph.png' />
+                <Item>
+                    <Item.Image size='small' src='/images/wireframe/image.png' />
+
+                    <Item.Content>
+                        <Item.Header as='a'>Cute Dog</Item.Header>
+                        <Item.Description>paragraph</Item.Description>
+                        <Item.Extra>
+                        <Icon color='green' name='check' /> 121 Votes
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
             </Grid.Column>
             <Grid.Column>
-                <Image src='/images/wireframe/media-paragraph.png' />
+                <Item>
+                    <Item.Image size='small' src='/images/wireframe/image.png' />
+
+                    <Item.Content>
+                        <Item.Header as='a'>Cute Dog</Item.Header>
+                        <Item.Description>paragraph</Item.Description>
+                        <Item.Extra>
+                        <Icon color='green' name='check' /> 121 Votes
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
             </Grid.Column>
         </Grid.Row>
 
         <Grid.Row>
             <Grid.Column>
-                <Image src='/images/wireframe/media-paragraph.png' />
+                <Item>
+                    <Item.Image size='small' src='/images/wireframe/image.png' />
+
+                    <Item.Content>
+                        <Item.Header as='a'>Cute Dog</Item.Header>
+                        <Item.Description>paragraph</Item.Description>
+                        <Item.Extra>
+                        <Icon color='green' name='check' /> 121 Votes
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
             </Grid.Column>
             <Grid.Column>
-                <Image src='/images/wireframe/media-paragraph.png' />
+                <Item>
+                    <Item.Image size='small' src='/images/wireframe/image.png' />
+
+                    <Item.Content>
+                        <Item.Header as='a'>Cute Dog</Item.Header>
+                        <Item.Description>paragraph</Item.Description>
+                        <Item.Extra>
+                        <Icon color='green' name='check' /> 121 Votes
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
             </Grid.Column>
             <Grid.Column>
-                <Image src='/images/wireframe/media-paragraph.png' />
+                <Item>
+                    <Item.Image size='small' src='/images/wireframe/image.png' />
+
+                    <Item.Content>
+                        <Item.Header as='a'>Cute Dog</Item.Header>
+                        <Item.Description>paragraph</Item.Description>
+                        <Item.Extra>
+                        <Icon color='green' name='check' /> 121 Votes
+                        </Item.Extra>
+                    </Item.Content>
+                </Item>
             </Grid.Column>
         </Grid.Row>
   </Grid>
