25d08339a54a1ce571bf986178e72ba784347fa5
Alexandra <alexandraneff@live.com>
2023-07-17 09:13:32 -0500

    added grid styling to ItemList

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Menu, Header } from 'semantic-ui-react'
+import { Menu, Header, Input } from 'semantic-ui-react'
 
 function NavBar() {
     return (
 <div>
 <Menu pointing secondary size='large'>
-<Menu.Item as={NavLink} to='/' name='home' />
-<Menu.Item as={NavLink} to='/my_items' name='my items' />
-<Menu.Item as={NavLink} to='/new' name='new review' />
+    <Menu.Item as={NavLink} to='/' name='home' />
+    <Menu.Item as={NavLink} to='/my_items' name='my items' />
+    <Menu.Item as={NavLink} to='/new' name='new review' />
+    <Menu.Menu position='right'>
+    <Menu.Item>
+        <Input icon='search' placeholder='Search...' />
+    </Menu.Item>
+    <Menu.Item as={NavLink} to='/logout' name='logout' />
+    </Menu.Menu>
 </Menu>
 </div>
     )
--- client/src/pages/ItemList.js
+++ client/src/pages/ItemList.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Grid, Image, Item } from 'semantic-ui-react'
 
 function ItemList() {
+    return (
+    <Grid columns={3} divided>
+        <Grid.Row>
+            <Grid.Column>
+                <Image src='/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+        </Grid.Row>
 
+        <Grid.Row>
+            <Grid.Column>
+                <Image src='/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/images/wireframe/media-paragraph.png' />
+            </Grid.Column>
+        </Grid.Row>
+  </Grid>
+    )
 }
 
 export default ItemList 
