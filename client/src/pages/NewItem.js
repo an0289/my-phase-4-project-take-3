dc0ea251ce9546dc23f0b72cae401bf70f387985
Alexandra <alexandraneff@live.com>
2023-07-17 09:39:31 -0500

    added form to 'new item'

--- client/src/pages/NewItem.js
+++ client/src/pages/NewItem.js
@@ -1,7 +1,21 @@
 import React from 'react'
-// This will likely change to 'NewItem' so that a review can be submitted directly from the Home page
-function NewItem() {
+import { Button, Form, Segment, Divider } from 'semantic-ui-react'
 
+function NewItem() {
+    return (
+    <>
+    <Divider hidden />
+    <Segment inverted>
+        <Form inverted>
+            <Form.Group widths='equal'>
+                <Form.Input fluid label='First name' placeholder='First name' />
+                <Form.Input fluid label='Last name' placeholder='Last name' />
+            </Form.Group>
+            <Button type='submit'>Submit</Button>
+        </Form>
+    </Segment>
+    </>
+    )
 }
 
 export default NewItem
