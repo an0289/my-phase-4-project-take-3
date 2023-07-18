f6c443f9ea69a7fbbd117b6ff012c0b760d16c25
Alexandra <alexandraneff@live.com>
2023-07-18 16:29:43 -0500

    added content to controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,16 @@
 class ItemsController < ApplicationController
+    def index 
+        render json: Item.all
+    end 
+
+    def show 
+        item = find_item 
+        render json: item 
+    end 
+
+    private 
+
+    def find_item
+        Item.find(params[:id])
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,39 @@
 class ReviewsController < ApplicationController
+
+    def index 
+        render json: Review.all 
+    end
+
+    def show 
+        review = find_review 
+        render json: review 
+    end 
+
+    def create 
+        review = @current_user.reviews.create!(review_params)
+        render json: review, status: :created 
+    end
+    
+    def update
+        review = find_review
+        review.update 
+        render json: review 
+    end 
+
+    def destroy 
+        review = find_review 
+        review.destroy 
+        head :no_content 
+    end 
+
+    private 
+
+    def review_params
+        params.permit(:title, :body)
+    end 
+
+    def find_review
+        Review.find(params[:id])
+    end 
+
 end
