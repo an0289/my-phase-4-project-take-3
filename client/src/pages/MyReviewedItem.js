ae4988ec663a5fb400c2644583c58b3257820ebd
Alexandra <alexandraneff@live.com>
2023-07-23 16:33:37 -0500

    created an 'edit review' form

--- client/src/pages/MyReviewedItem.js
+++ client/src/pages/MyReviewedItem.js
@@ -1,21 +1,55 @@
-import React from 'react'
-import { Grid, Image, Item, Icon, Card, Segment, Divider, Header } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Grid, Image, Item, Icon, Card, Segment, Divider, Header, Button, Form, TextArea, Input } from 'semantic-ui-react'
 
 function MyReviewedItem({ review }) {
+
+    const [isEdit, setIsEdit] = useState(false)
     return (
         <Grid.Column stretched>
-                <Segment color='violet'>
-                <Item.Group>
-                    <Item>
-                    <Header as='h4'>{review.name}</Header>
-                        <Item.Image size='tiny' src={review.item_image} />
-                        <Item.Content>
-                            <Item.Header>{review.title}</Item.Header>
-                            <Item.Description>{review.body}</Item.Description>
-                        </Item.Content>
-                    </Item>
-                </Item.Group>
-                </Segment>
+            {isEdit ? (
+             <>
+             <Segment color='violet'>
+             <Item.Group>
+                 <Item>
+                 <Header as='h4'>{review.name}</Header>
+                     <Item.Image size='tiny' src={review.item_image} />
+                     <Form>
+                         <Form.Field
+                         control={Input} 
+                         label='Review Title'
+                         placeholder='Title'
+                         />
+                         <Form.Field 
+                         control={TextArea} 
+                         label='Review Body'
+                         placeholder='Body'
+                         />
+                         <Button as='submit' size='tiny' inverted color='green' floated='right' >Submit Edit</Button>
+                     </Form>
+                 </Item>
+             </Item.Group>
+             <Button onClick={() => setIsEdit(false)} size='tiny' inverted color='red' floated='left' >Cancel Edit</Button>
+             </Segment>
+            </>
+            ) : (
+            <>
+            <Segment color='violet'>
+            <Item.Group>
+                <Item>
+                <Header as='h4'>{review.name}</Header>
+                    <Item.Image size='tiny' src={review.item_image} />
+                    <Item.Content>
+                        <Item.Header>{review.title}</Item.Header>
+                        <Item.Description>{review.body}</Item.Description>
+                    </Item.Content>
+                </Item>
+            </Item.Group>
+            <Button size='tiny' inverted color='red' floated='right' >Delete Review</Button>
+            <Button onClick={() => setIsEdit(true)} size='tiny' inverted color='blue' floated='right' >Edit Review</Button>
+            </Segment>
+            </>
+            )}
+           
                 <Divider hidden/>
             </Grid.Column>
     )
